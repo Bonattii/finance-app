a868ab404ed35d9931873b201550d0cfa3f013b7
Rodrigo Bonatti <rodrigo@Rodrigos-MacBook-Pro.local>
2024-06-09 15:54:34 -0300

    Create Header

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,23 +1,12 @@
-import { z } from 'zod'
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import { zValidator } from '@hono/zod-validator'
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', clerkMiddleware(), (c) => {
-  const auth = getAuth(c)
-
-  if (!auth?.userId) {
-    return c.json({ error: 'Unauthorized' })
-  }
-
-  return c.json({
-    message: 'Hello World!'
-  })
+app.get('/hello', (c) => {
+  return c.json({ hello: 'World' })
 })
 
 export const GET = handle(app)
