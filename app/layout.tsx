6f03d598544625814ec58179428e01622aab08dd
Rodrigo Bonatti <rodrigo@Rodrigos-MacBook-Pro.local>
2024-07-20 22:22:55 -0300

    Create get accounts route and react-query hook

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-
 import { ClerkProvider } from '@clerk/nextjs'
+
+import { QueryProvider } from '@/providers/query-provider'
 
 import './globals.css'
 
@@ -20,7 +21,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <QueryProvider>{children}</QueryProvider>
+        </body>
       </html>
     </ClerkProvider>
   )
