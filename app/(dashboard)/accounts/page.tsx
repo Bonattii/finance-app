1a85034b239708924a578171635fdc79d29ef8ec
Rodrigo Bonatti <rodrigo@192.168.2.21>
2024-08-06 15:12:11 -0300

    Create accounts bulk delete functionality

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -7,14 +7,18 @@ import { DataTable } from '@/components/data-table'
 import { Skeleton } from '@/components/ui/skeleton'
 import { useNewAccount } from '@/features/accounts/hooks/use-new-account'
 import { useGetAccounts } from '@/features/accounts/api/use-get-accounts'
+import { useBulkDeleteAccount } from '@/features/accounts/api/use-bulk-delete'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 import { columns } from './columns'
 
 const AccountsPage = () => {
   const newAccount = useNewAccount()
+  const deleteAccounts = useBulkDeleteAccount()
   const accountsQuery = useGetAccounts()
   const accounts = accountsQuery.data || []
+
+  const isDisabled = accountsQuery.isLoading || deleteAccounts.isPending
 
   if (accountsQuery.isLoading) {
     return (
@@ -51,8 +55,11 @@ const AccountsPage = () => {
             columns={columns}
             data={accounts}
             filterKey="email"
-            onDelete={() => {}}
-            disabled
+            onDelete={(row) => {
+              const ids = row.map((r) => r.original.id)
+              deleteAccounts.mutate({ ids })
+            }}
+            disabled={isDisabled}
           />
         </CardContent>
       </Card>
