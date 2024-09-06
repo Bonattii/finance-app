4ae33317ba6ede6af91ff9862420697f4c4dc900
Rodrigo Bonatti <rodrigo@Rodrigos-MacBook-Pro.local>
2024-09-05 21:41:27 -0300

    Create date & account filters

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,7 @@
 import { Loader2 } from 'lucide-react'
 import { UserButton, ClerkLoading, ClerkLoaded } from '@clerk/nextjs'
 
+import { Filters } from '@/components/filters'
 import { Navigation } from '@/components/navigation'
 import { HeaderLogo } from '@/components/header-logo'
 import { WelcomeMsg } from '@/components/welcome-msg'
@@ -24,6 +25,7 @@ export const Header = () => {
         </div>
 
         <WelcomeMsg />
+        <Filters />
       </div>
     </header>
   )
